246e7fe4d1037391e34de97c79bb8de3eaf5280f
ved <ved28592@gmail.com>
2019-10-13 14:02:57 -0400

    Bugfix for finding kernel.bin and loader.bin

--- src/utils/Pintos.pm
+++ src/utils/Pintos.pm
@@ -359,7 +359,8 @@ sub cyl_sectors {
 # Makes sure that the loader is a reasonable size.
 sub read_loader {
     my ($name) = @_;
-    $name = find_file ("../threads/build/loader.bin") if !defined $name;
+    my $pdir = $ENV{PINTOSDIR}."/src/threads/build/loader.bin";
+    $name = find_file ($pdir) if !defined $name;
     die "Cannot find loader\n" if !defined $name;
 
     my ($handle);
